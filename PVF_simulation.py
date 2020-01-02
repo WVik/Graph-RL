f203bd272921f8bea0931f6bccf361bfbde9181f
Vikram Waradpande <wvikram11@gmail.com>
2020-01-02 11:53:22 +0100

    fixed syntax

--- PVF_simulation.py
+++ PVF_simulation.py
@@ -80,7 +80,7 @@ def simulate(num_states, reward_location, walls_location, maze, learned_policy, 
                 action = learned_policy.select_action(
                     maze.domain.current_state())
                 sample = maze.domain.apply_action(action)
-                print(sample.next_state, end='')
+                print(sample.next_state)
                 absorb = sample.absorb
                 steps_to_goal += 1
                 samples.append(sample)
