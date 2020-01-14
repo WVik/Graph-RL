a266926971ba8c71534daacc3b02300f1f11019f
Vikram Waradpande <wvikram11@gmail.com>
2020-01-14 12:46:47 +0100

    Restore learn_node2vec_basis

--- learning_maze.py
+++ learning_maze.py
@@ -112,37 +112,20 @@ class LearningMazeDomain():
             samples.append(sample)
 
         return steps_to_goal, learned_policy, samples, distances
-
-    def learn_node2vec_basis(self, maze=None, dimension=NUM_BASIS, walk_length=30, num_walks=10, window_size=10,
+        
+        
+    def learn_node2vec_basis(self, maze=None,dimension=NUM_BASIS, walk_length=30, num_walks=10, window_size=10,
                              p=1, q=1, epochs=1, discount=DISCOUNT, explore=EXPLORE, max_iterations=MAX_ITERATIONS,
-                             max_steps=NUM_SAMPLES, initial_policy=None, edgelist='lspi/graph_10_demo'):
-
+                             max_steps=NUM_SAMPLES, initial_policy=None, edgelist ='lspi/graph_10_maze'):
+                             
         max_steps = 0
 
         if initial_policy is None:
             initial_policy = lspi.Policy(self.domain, lspi.basis_functions.Node2vecBasis(
                 edgelist, num_actions=4, transition_probabilities=self.domain.transition_probabilities,
-                dimension=dimension, walk_length=walk_length, num_walks=num_walks, window_size=window_size,
+                dimension=dimension,walk_length=walk_length, num_walks=num_walks, window_size=window_size,
                 p=p, q=q, epochs=epochs), discount, explore)
 
-        learned_policy, distances = lspi.learn(maze, initial_policy, self.solver,
-                                               max_iterations=max_iterations)
 
-        self.domain.reset()
-
-        steps_to_goal = 0
-        absorb = False
-        samples = []
-        print("learn")
-        while (not absorb) and (steps_to_goal < max_steps):
-            action = learned_policy.select_action(self.domain.current_state())
-            sample = self.domain.apply_action(action)
-            absorb = sample.absorb
-            if absorb:
-                print('Reached the goal in %d', steps_to_goal)
-            steps_to_goal += 1
-
-            samples.append(sample)
-
-        print("learnt")
-        return steps_to_goal, learned_policy, samples, distances
+        #Insert the neural network model
+        return initial_policy.basis.model
